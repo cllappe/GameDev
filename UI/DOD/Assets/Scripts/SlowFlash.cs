0fa6449c758c33b17dff5dd9f71ed2307cb47d11
monicaralvarezpadilla <38265031+monicaralvarezpadilla@users.noreply.github.com>
2018-09-25 16:08:16 -0500

    UI & Intro

---
+++ UI/DOD/Assets/Scripts/SlowFlash.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SlowFlash : MonoBehaviour
+{
+
+    private Text text;
+    public float time;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Sin(Time.time * time));
+    }
+}
---
+++ UI/DOD/Assets/Scripts/startTorch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startTorch : MonoBehaviour {
+
+    public Animator animation;
+    public float time;
+
+    void Start()
+    {
+        animation = GetComponent<Animator>();
+        StartCoroutine(AnimationDelay());
+    }
+
+    IEnumerator AnimationDelay()
+    {
+        yield return new WaitForSeconds(time);
+        animation.Play("TorchStart");
+    }
+}
