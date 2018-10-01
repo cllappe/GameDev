faf462bd0dfc654f338201813502dd594081c4b0
monicaralvarezpadilla <38265031+monicaralvarezpadilla@users.noreply.github.com>
2018-10-01 01:40:42 -0500

    UI Save in case I break something
    Changes:
    -MainMenu: New Game and Quit Functionality
    
    Save before Attempting to Combine Dungeon & UI

--- UI/DOD/Assets/Scripts/SlowFlash.cs
+++ UI/DOD/Assets/Scripts/SlowFlash.cs
@@ -7,16 +7,26 @@ public class SlowFlash : MonoBehaviour
 
     private Text text;
     public float time;
+    public float fireDelay = 3.0f;
 
+    private float fireTimestamp = 0.0f;
     // Use this for initialization
     void Start()
     {
         text = gameObject.GetComponent<Text>();
+        fireTimestamp = Time.realtimeSinceStartup + fireDelay;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Sin(Time.time * time));
+
+
+        if (Time.realtimeSinceStartup > fireTimestamp)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Sin(Time.time * time));
+        }
+       // text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Sin(Time.time * time));
     }
 }
