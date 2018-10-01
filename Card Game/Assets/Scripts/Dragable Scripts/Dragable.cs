031bf3874de4c76108b815932dbf254402740f98
Chelsea Lappe <cllappe@uh.edu>
2018-10-01 17:02:30 -0500

    Being annoying with saves because I'm scared

--- Card Game/Assets/Scripts/Dragable Scripts/Dragable.cs
+++ Card Game/Assets/Scripts/Dragable Scripts/Dragable.cs
@@ -28,93 +28,101 @@ public class Dragable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
         if (!Input.GetMouseButton(0))
         {
             startPos = transform.position;
-            //iTween.MoveTo(gameObject, new Vector3(transform.position.x - 0.5f, transform.position.y + 1.2f, transform.position.z - 1.2f), 1f);
+            iTween.MoveTo(gameObject, new Vector3(transform.position.x - 0.5f, transform.position.y + 1.2f, transform.position.z - 1.2f), 1f);
         }
     }
 
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        dragging = true;
+        if (playerTurn)
+        {
+            dragging = true;
 
-        placeholder = new GameObject();
-        placeholder.transform.SetParent(this.transform.parent);
-        LayoutElement le = placeholder.AddComponent<LayoutElement>();
-        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
-        le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
-        le.flexibleWidth = 0;
-        le.flexibleHeight = 0;
+            placeholder = new GameObject();
+            placeholder.transform.SetParent(this.transform.parent);
+            LayoutElement le = placeholder.AddComponent<LayoutElement>();
+            le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+            le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+            le.flexibleWidth = 0;
+            le.flexibleHeight = 0;
 
-        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+            placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
 
-        parentToReturnTo = this.transform.parent;
-        placeholderParent = parentToReturnTo;
-        this.transform.SetParent(this.transform.parent.parent);
+            parentToReturnTo = this.transform.parent;
+            placeholderParent = parentToReturnTo;
+            this.transform.SetParent(this.transform.parent.parent);
 
-        GetComponent<CanvasGroup>().blocksRaycasts = false;
-        mouseOffset = Vector3.Distance(this.transform.position, Camera.main.transform.position);
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
+            mouseOffset = Vector3.Distance(this.transform.position, Camera.main.transform.position);   
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Vector3 rayPoint = ray.GetPoint(mouseOffset);
-        rayPoint.z = 0;
-        this.transform.position = rayPoint;
+        if (playerTurn)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(mouseOffset);
+            rayPoint.z = 0;
+            this.transform.position = rayPoint;
         
 
-        if (placeholder.transform.parent != placeholderParent)
-            placeholder.transform.SetParent(placeholderParent);
+            if (placeholder.transform.parent != placeholderParent)
+                placeholder.transform.SetParent(placeholderParent);
 
-        int newSiblingIndex = placeholderParent.childCount;
+            int newSiblingIndex = placeholderParent.childCount;
 
-        for (int i = 0; i < placeholderParent.childCount; i++)
-        {
-            if (this.transform.position.x < placeholderParent.GetChild(i).position.x)
+            for (int i = 0; i < placeholderParent.childCount; i++)
             {
+                if (this.transform.position.x < placeholderParent.GetChild(i).position.x)
+                {
 
-                newSiblingIndex = i;
+                    newSiblingIndex = i;
 
-                if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
-                    newSiblingIndex--;
+                    if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+                        newSiblingIndex--;
 
-                break;
+                    break;
+                }
             }
-        }
 
-        placeholder.transform.SetSiblingIndex(newSiblingIndex);
+            placeholder.transform.SetSiblingIndex(newSiblingIndex);  
+        }
 
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (validDrop){
-            //Debug.Log("In Valid Drop");
-            startPos = this.transform.position;
-            validDropHappened = true;
-            this.transform.SetParent(parentToReturnTo);
-            this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
-            foreach (Transform child in transform) {
-                GameObject.Destroy(child.gameObject);
+        if (playerTurn)
+        {
+            if (validDrop){
+                //Debug.Log("In Valid Drop");
+                startPos = this.transform.position;
+                validDropHappened = true;
+                this.transform.SetParent(parentToReturnTo);
+                this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
+                foreach (Transform child in transform) {
+                    GameObject.Destroy(child.gameObject);
+                }
             }
-        }
-        else{
-            startPos = new Vector3(this.transform.position.x, startPos.y, startPos.z);
-            this.transform.SetParent(parentToReturnTo);
-            this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
-        }
-        GetComponent<CanvasGroup>().blocksRaycasts = true;
+            else{
+                startPos = new Vector3(this.transform.position.x, startPos.y, startPos.z);
+                this.transform.SetParent(parentToReturnTo);
+                this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
+            }
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
 
-        dragging = false;
-        Destroy(placeholder);
+            dragging = false;
+            Destroy(placeholder);   
+        }
     }
 
     private void OnMouseExit()
     {
-        if (!Input.GetMouseButton(0) && !validDropHappened)
+        if (!validDropHappened)
         {
-            //iTween.MoveTo(gameObject, startPos, 0f);
+            iTween.MoveTo(gameObject, startPos, 0f);
         }
 
         validDrop = false;
