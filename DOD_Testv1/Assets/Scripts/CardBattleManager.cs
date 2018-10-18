743b267e36482a68445ef6e53ffde2a8f0ac1cbc
monicaralvarezpadilla <38265031+monicaralvarezpadilla@users.noreply.github.com>
2018-10-18 16:58:44 -0500

    Battlev2.0 4K

---
+++ DOD_Testv1/Assets/Scripts/CardBattleManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Guirao.UltimateTextDamage;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class CardBattleManager : MonoBehaviour {
+
+    public CardBattleManager instance;
+
+    public GameObject cardPrefab;
+
+    public Deck deck;
+
+    public Canvas hand;
+
+    public List<Card> inUseDeck;
+    
+    public List<Character> charOrder;
+
+    public static int deadEnemies = 0;
+
+    public Text turnMonitor;
+
+    public Text DeckCounter;
+
+    public static bool enemyTurn = false;
+
+    public static bool draw1card;
+
+    public GameObject dragBlock;
+
+    private void Awake()
+    {
+        if (instance == null){
+            instance = this;
+        }
+        else{
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+       
+    }
+
+    private void Start()
+    {
+        dragBlock = GameObject.Find("Hand Block");
+        turnMonitor = gameObject.GetComponent<Text>();
+        Dragable.playerTurn = false;
+        for (int j = 0; j < 25; j++)
+        {
+            inUseDeck.Add(deck.cards[j]);
+        }
+        for (int i = 0; i < 5; i++){
+            GameObject go = Instantiate(cardPrefab) as GameObject;
+            CardDisplay display = go.GetComponent<CardDisplay>();
+            Dragable dragable = go.GetComponent<Dragable>();
+            int randomSelectedCard = Random.Range(0, inUseDeck.Count);
+            display.CardSetup(inUseDeck[randomSelectedCard]);
+            inUseDeck.RemoveAt(randomSelectedCard);
+            display.transform.SetParent(hand.transform.GetChild(0), false);
+            dragable.parentToReturnTo = display.transform.parent;
+            dragable.placeholderParent = display.transform.parent;
+            dragable.targets = display.cardTargets;
+        }
+    }
+    private void Update()
+    {
+        DeckCounter.text = "Remaining Cards =" + inUseDeck.Count;
+        if (draw1card)
+        {
+            drawACard();
+            draw1card = false;
+        }
+
+        if (Dragable.playerTurn)
+        {
+            dragBlock.SetActive(false);
+        }
+        else
+        {
+            dragBlock.SetActive(true);
+        }
+    }
+
+    public void setTurnText(String thisText)
+    {
+        turnMonitor.text = thisText;
+    }
+
+    public void drawACard()
+    {
+        if (hand.transform.GetChild(0).childCount < 5){
+            GameObject go = Instantiate(cardPrefab) as GameObject;
+            CardDisplay display = go.GetComponent<CardDisplay>();
+            Dragable dragable = go.GetComponent<Dragable>();
+            int randomSelectedCard = Random.Range(0, inUseDeck.Count);
+            display.CardSetup(inUseDeck[randomSelectedCard]);
+            inUseDeck.RemoveAt(randomSelectedCard);
+            display.transform.SetParent(hand.transform.GetChild(0), false);
+            dragable.parentToReturnTo = display.transform.parent;
+            dragable.placeholderParent = display.transform.parent;
+            dragable.targets = display.cardTargets;
+        }
+        else
+        {
+            int randomSelectedCard = Random.Range(0, inUseDeck.Count);
+            inUseDeck.RemoveAt(randomSelectedCard);
+            GameObject PlayerUTD = GameObject.Find("PlayerUTD");
+            PlayerUTD.GetComponent<UltimateTextDamageManager>().Add("Over Drawn. Lost a Card.",PlayerUTD.transform,"status");
+        }
+    }
+}
---
+++ DOD_Testv1/Assets/Scripts/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName ="Character")]
+
+public class Character : ScriptableObject{
+    public enum Type
+    {
+        PLAYER,
+        ENEMY,
+        MINIBOSS,
+        BOSS
+    }
+
+    public Type charType;
+    public Sprite avatar;
+    public int health;
+    public int luck;
+    public int basicAttackDmg;
+    public GameObject healthBar;
+    public bool dmgReflected = false;
+    public float defence;
+    public int dmgMod;
+    public bool skipTurn = false;
+
+}
