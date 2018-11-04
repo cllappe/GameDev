ad499db2135e5e12e1d6d82b044395fa7d72a689
monicaralvarezpadilla <38265031+monicaralvarezpadilla@users.noreply.github.com>
2018-11-04 00:03:46 -0500

    Hi idk what is it doing

--- DOD_Testv1/Assets/Scripts/Enemies.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Enemy Array",menuName = "Create Enemy Array")]
-public class Enemies : ScriptableObject
-{
-    public Character[] enemies;
-}
