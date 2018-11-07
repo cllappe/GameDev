ee479ff79aa98c07ce2703b27e49ec80a4550cf9
Chelsea Lappe <cllappe@uh.edu>
2018-11-06 20:04:33 -0600

    Loading Screen

---
+++ DOD_Testv1/Assets/Editor/PlayerPrefsReset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class PlayerPrefsReset : EditorWindow {
+     
+    [MenuItem("Edit/Reset Playerprefs")]
+     
+    public static void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
---
+++ DOD_Testv1/Assets/Scripts/LoadingScreenControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenControl : MonoBehaviour
+{
+
+    public GameObject loadingScreenObject;
+    public Slider slider;
+
+    private AsyncOperation async;
+
+    public void LoadScreen(int loadIndex)
+    {
+        StartCoroutine(LoadingScreen(loadIndex));
+    }
+
+    IEnumerator LoadingScreen(int sceneIndex)
+    {
+        loadingScreenObject.SetActive(true);
+        async = SceneManager.LoadSceneAsync(sceneIndex);
+        async.allowSceneActivation = false;
+
+        while (async.isDone == false)
+        {
+            slider.value = async.progress;
+            if (async.progress == 0.9f)
+            {
+                slider.value = 1f;
+                async.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
